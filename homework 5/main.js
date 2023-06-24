489b429bde08ab251115bca6082c76b11273ccf9
Tetiana <taniakozhukharenko@gmail.com>
2023-06-24 21:59:26 +0300

    Changed 5 homework

--- homework 5/main.js
+++ homework 5/main.js
@@ -1,87 +1,96 @@
-/* 1.Створіть функцію getRandomArray(length, min, max) – яка повертає масив випадкових цілих чисел. У функції 
-є параметри: length - довжина масиву, min – мінімальне значення цілого числа, max – максимальне значення цілого 
-числа. Приклад: getRandomArray(15, 1, 100) –> [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2] */
+/* 1.Створіть функцію getRandomArray(length, min, max) – яка повертає масив випадкових цілих чисел. У функції є 
+параметри: length - довжина масиву, min – мінімальне значення цілого числа, max – максимальне значення цілого числа. */
 
 function getRandomArray(length, min, max) {
-  const randomArray = [];
+  const newArr = [];
+
   for(let i = 0; i < length; i++) {
-    let randomNumbers = Math.floor(Math.random(i) * (max - min + 1)) + min;
-    randomArray.push(randomNumbers);
+    let rNumber = Math.floor(Math.random() * (max - min + 1) + min);
+    newArr.push(rNumber);
   }
-  return randomArray;
+
+  return newArr;
 }
 
-console.log(getRandomArray(15, 1, 100));
+const randomNum = getRandomArray(15, 3, 100);
+console.log(randomNum);
 
-/* 2.Створіть функцію getAverage(...numbers) – яка рахує середнє арифметичне всіх переданих в неї аргументів. НЕ ЦІЛІ 
-ЧИСЛА ІГНОРУЮТЬСЯ. Приклад: getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) –> 34.4 */
+
+/* 3.Створіть функцію getAverage(...numbers) – яка рахує середнє арифметичне всіх переданих в неї аргументів. 
+Не цілі числа мають ігноруватися. */
 
 function getAverage(...numbers) {
-  let res = 0;
-  let count = 0;
-  let result;
-  for(let i = 0; i < numbers.length; i++) {
-    if (Number.isInteger(numbers[i])) {
-      res += numbers[i];
-      count++
+  let sum = 0;
+
+  numbers.forEach(el => {
+    if(Number.isInteger(el)) {
+      sum += el;
     }
-  }
-  result = (res / count);
-  return result;
+  })
+
+  return sum / numbers.length;
 }
 
-console.log(getAverage(1, 1, 3.9, 4, 9, 9.8));
+const averageValue = getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2);
+console.log(averageValue);
 
-/* 3.Створіть функцію filterEvenNumbers(...numbers) – яка фільтрує парні числа передані як аргументи функції.
+// 4.Створіть функцію getMedian(...numbers) – яка рахує медіану всіх переданих в неї аргументів. Не цілі числа мають ігноруватися.
+
+function getMedian(...numbers) {
+  const integers = numbers.filter(el => Number.isInteger(el));
+  const sorted = integers.sort((a, b) => a - b);
+  const middleIndex = Math.floor((sorted.length / 2));
+
+  if (sorted.length % 2 == 0) {
+    return(sorted[middleIndex -1] + sorted[middleIndex]) / 2;
+  } else {
+    return sorted[middleIndex];
+  }
+}
+
+const medianNum = getMedian(1, 2, 3, 4, 5);
+console.log(medianNum);
+
+/* 5.Створіть функцію filterEvenNumbers(...numbers) – яка фільтрує парні числа передані як аргументи функції.
 Приклад: filterEvenNumbers(1, 2, 3, 4, 5, 6) -> [1, 3, 5] */
 
 function filterEvenNumbers(...numbers) {
-  const res = numbers.filter((number) => {
-    return number % 2 !== 0;
-  });
-  return res;
+
+  const oddNum = numbers.filter(el => {
+    return el % 2 !== 0;
+  })
+
+  return oddNum;
 }
 
-console.log(filterEvenNumbers(1, 2, 3, 4, 5, 6));
+const oddNumbers = filterEvenNumbers(1, 2, 3, 4, 5, 6);
+console.log(oddNumbers);
 
-/* 4.Створіть функцію countPositiveNumbers(...numbers) – яка порахує кількість чисел більших. Приклад: 
-countPositiveNumbers(1, -2, 3, -4, -5, 6) -> 3 */
+// 6.Створіть функцію countPositiveNumbers(...numbers) – яка порахує кількість чисел більших за 0.
 
 function countPositiveNumbers(...numbers) {
-  const resNumber = numbers.filter((item) => {
-    return item > 0;
-  });
-  return resNumber.length;
+  
+  const numPos = numbers.filter( el => {
+    return el > 0;
+  })
+
+  return numPos.length;
 }
 
-console.log(countPositiveNumbers(1, -2, 3, -4, -5, 6));
+const positiveNum = countPositiveNumbers(1, -2, 3, -4, -5, 6);
+console.log(positiveNum);
 
-/* 5.Створіть функцію getDividedByFive(...numbers) – яка відфільтрує усі елементи в масиві та залишить тільки ті, 
-які діляться на ціло на 5. Приклад: getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) -> [55, 55] */
+/* 7.Створіть функцію getDividedByFive(...numbers) – яка відфільтрує усі елементи в масиві та залишить тільки ті, 
+які діляться на ціло на 5. */
 
 function getDividedByFive(...numbers) {
-  const resNumbers = numbers.filter((number) => {
-    return Number.isInteger(number / 5);
-  });
-  return resNumbers;
+
+  const dividedBy5 = numbers.filter(el => {
+    return el % 5 == 0;
+  })
+
+  return dividedBy5;
 }
 
-console.log(getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
-
-/* 6. Створіть функцію divideByThree(word), яка розбиває кожне слово на умовні склади по 3 букви. Якщо букв менше трьох 
-– не розбиває. Пробіли завжди видаляються. Рядок приводится до нижнього регістру. Приклад: divideByThree("Commander) -> 
-["com", "man", "der"] Приклад: divideByThree("live") -> ["liv", "e"] */
-
-function divideByThree(word) {
-  const result = [];
-  const lowerCaseWord = word.toLowerCase().replace(/\s/g, '');
-  for (let i = 0; i < lowerCaseWord.length; i += 3) {
-    if(lowerCaseWord.length < 3) {
-      return "Слово замале для того, щоб розбивати його на склади по 3 букви.";
-    }
-    result.push(lowerCaseWord.slice(i, i + 3));
-  }
-  return result;
-}
-
-console.log(divideByThree("Commander"));
+const dividedByFive = getDividedByFive(6, 2, 55, 11, 78, 10, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2, 5);
+console.log(dividedByFive);
